511b794924c6cf99e81142a142f89a929a3bbbf3
Marco <aurelio.marco@huhsp.org.br>
2026-02-08 12:44:43 -0300

    Transformando app de console em Web App com Flask

--- app.py
+++ app.py
@@ -1,53 +1,36 @@
+from flask import Flask, render_template, request, redirect
 import banco
-import sys
 
-def exibir_menu():
-    print("\n" + "="*30)
-    print("   GESTÃO DE SÓCIOS - CLUBE   ")
-    print("="*30)
-    print("1. Cadastrar Sócio")
-    print("2. Listar Sócios")
-    print("3. Atualizar Status (Ativar/Inativar)")
-    print("4. Remover Sócio")
-    print("5. Sair")
-    print("-" * 30)
+app = Flask(__name__)
 
-def main():
-    # Garante que a tabela existe ao iniciar
-    banco.criar_tabela()
+# Garante que a tabela existe
+banco.criar_tabela()
+
+@app.route('/')
+def index():
+    # Pega os sócios do banco
+    lista_socios = banco.listar_socios()
+    # Mostra o arquivo HTML enviando a lista para ele
+    return render_template('index.html', socios=lista_socios)
+
+@app.route('/cadastro')
+def cadastro():
+    return render_template('cadastro.html')
+
+@app.route('/criar', methods=['POST'])
+def criar():
+    # Pega os dados digitados no formulário do navegador
+    nome = request.form['nome']
+    cpf = request.form['cpf']
+    plano = request.form['plano']
     
-    while True:
-        exibir_menu()
-        opcao = input("Escolha uma opção: ")
+    banco.adicionar_socio(nome, cpf, plano)
+    return redirect('/')
 
-        if opcao == '1':
-            nome = input("Nome completo: ")
-            cpf = input("CPF (apenas números): ")
-            plano = input("Plano (Gold/Silver): ")
-            banco.adicionar_socio(nome, cpf, plano)
-            
-        elif opcao == '2':
-            socios = banco.listar_socios()
-            print("\n--- LISTA DE SÓCIOS ---")
-            print(f"{'ID':<5} {'NOME':<20} {'CPF':<15} {'PLANO':<10} {'STATUS'}")
-            for s in socios:
-                status = "Ativo" if s[4] == 1 else "Inativo"
-                print(f"{s[0]:<5} {s[1]:<20} {s[2]:<15} {s[3]:<10} {status}")
-                
-        elif opcao == '3':
-            id_socio = input("ID do sócio: ")
-            status = input("Novo status (1 para Ativo, 0 para Inativo): ")
-            banco.atualizar_status(id_socio, status)
+@app.route('/deletar/<int:id>')
+def deletar(id):
+    banco.deletar_socio(id)
+    return redirect('/')
 
-        elif opcao == '4':
-            id_socio = input("ID do sócio para remover: ")
-            banco.deletar_socio(id_socio)
-
-        elif opcao == '5':
-            print("Saindo do sistema...")
-            sys.exit()
-        else:
-            print("Opção inválida!")
-
-if __name__ == "__main__":
-    main()
+if __name__ == '__main__':
+    app.run(debug=True)
